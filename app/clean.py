d69061795131af699bcd1f7a99c70eb9c149cd02
Julen Badiola <33113693+julenbadiola@users.noreply.github.com>
2021-12-14 13:02:23 +0100

    Finished

--- app/clean.py
+++ app/clean.py
@@ -1,11 +1,8 @@
 from app.google import get_files, delete_file
-from app.model import DriveAsset
 
-def clean(db):
+def clean():
     print("Syncing (cleaning)")
     # Delete all files in drive that are not attached to any DriveAsset obj
     for i in get_files():
-        obj = db.query(DriveAsset).filter(DriveAsset.drive_id == i["id"]).first()
-        if not obj:
-            delete_file(i["id"])
+        delete_file(i["id"])
     return get_files()
--- app/database.py
+++ app/database.py
@@ -1,8 +1,5 @@
-import os
 import motor.motor_asyncio
 from app.config import settings
 
-print("bUSCA")
-print(settings.MONGODB_URL)
 client = motor.motor_asyncio.AsyncIOMotorClient(settings.MONGODB_URL)
 db = client.assets
