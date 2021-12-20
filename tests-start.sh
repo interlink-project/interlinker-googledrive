38e3f51f7a1cb4e6660394d04a5be23a0731ab6e
Julen Badiola <33113693+julenbadiola@users.noreply.github.com>
2021-12-20 11:18:34 +0100

    Image names dev / prod

--- tests-start.sh
+++ tests-start.sh
@@ -7,7 +7,7 @@ if [ $(uname -s) = "Linux" ]; then
 fi
 
 docker-compose down -v --remove-orphans # Remove possibly previous broken stacks left hanging after an error
-docker-compose build
-docker-compose up -d
+docker-compose -f docker-compose.yml -f docker-compose.solodev.yml build
+docker-compose -f docker-compose.yml -f docker-compose.solodev.yml up -d
 #docker-compose exec googledrive pytest --cov=app --cov-report=term-missing app/tests
 docker-compose exec googledrive pytest app/tests
